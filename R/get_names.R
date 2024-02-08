2ce8ad6fad83d286eb045b0a6b7ef8aff3313bee
Tjebo Heeren <tjebo@Tjebos-MacBook-Pro.local>
2024-02-08 10:01:56 -0300

    Fix issue #1. Moved functions out of read_maia_tgz and simplified data extraction (less looping, as list.dirs does find all xml and json files recursively). Cleaning data more streamlined.

--- R/get_names.R
+++ R/get_names.R
@@ -1,40 +1,5 @@
-#' get_names
-#' Extract patient identifiable information from MAIA raw data
-#' @name  get_names
-#' @description Internal function to extract name, dob and and sex from MAIA observers.
-#' This data is stored in json files attached to the microperimetry files.
-#' It is generally not recommended to use this function explicitely in order to keep personal identifiable data as safe as possible.
-#' @import dplyr
-#' @author Tjebo
-#' @param folder source folder which is searched for .tgz files. Default: workdirectory
-#' @return Data frame
-#'
-get_names <- function(folder = getwd()){
-  tgz_files <- get_tgz_files(folder = folder)
 
-  pull_out <- function(tgz_element)  {
-    #tgz_element is element of tgz_files (each tgz file), and pull_out will be used with lapply to perform the whole lot on every tgz file
-
-    tmpdir <- tempdir()
-    patID <- stringr::str_extract(tgz_element, "(?i)(?<=patient)\\d+")
-    utils::untar(tgz_element, exdir = tmpdir)
-    tmpdir_patID <- paste0(tmpdir,'/',patID)
-    ##dir of the files within the patID folder, makes object patID for the following code
-
-    ## makes list of json data, contains patient details
-    JsonData <- jsonlite::fromJSON(txt = paste0(tmpdir,"/",patID,"/",patID,".json"))
-
-    ## list2: what you want to pull out of the .json file
-    list2 <- list(patID = 'id', name = 'lastname', first_name = 'firstname', sex = 'gender', DOB = 'birthdate')
-
-    data.frame(lapply(list2, function(z) JsonData[[1]][[z]]), stringsAsFactors = FALSE)
-
-  }  ##end of pull_out() function to use for list of tgz files
-
-  data_names <- bind_rows(lapply(tgz_files, pull_out)) %>%
-    mutate(sex = case_when(sex == 'male' ~ 'm',
-                           sex == 'female' ~ 'f',
-                           TRUE ~ NA_character_))
-  return(data_names)
-}
-
+# to move into test_that etc
+# ls_json <- read_maia_tgz2(folder = file.path(getwd(), "data-raw"))
+#
+# extract_pii(ls_json[[1]])
--- R/read_maia_tgz.R
+++ R/read_maia_tgz.R
@@ -6,7 +6,7 @@
 #'
 #' @param folder source folder which is searched for .tgz files. Default: workdirectory
 #' @param incomplete set TRUE, if you want to see incomplete exams too
-#' @param timeclass either 'date' (date class) or 'datetime' (POSIXct class), for date and time of test
+#' @param timeclass either 'date' (Date class, default) or 'datetime' (POSIXct), how to provide microperimetry test time
 #' @return Data frame
 #' @import dplyr
 #' @import tidyr
@@ -15,104 +15,121 @@
 #' # read_maia_tgz(folder = file.path(getwd(), "data-raw"))
 #' @export
 
-read_maia_tgz2 <- function(folder = getwd(), incomplete = FALSE, timeclass = "datetime") {
+## Function to extract test data from maia files (xml and json)
+read_maia_tgz <- function(folder = getwd(), incomplete = FALSE, timeclass = "date") {
   # list of tgz files
-  tgz_name <- get_tgz_files(folder = folder)
-    # check if any tgz file exists
-  if (identical(tgz_name, character(0))) {
-    stop("No tgz file in this directory")
-  }
-  # pull_out will be lapplied on the list of tgz files
-  pull_out <- function(tgz_element) { # tgz_element is element of tgz_name (each tgz file)
-    tmpdir <- tempdir()
-    patID <- stringr::str_extract(tgz_element, "(?i)(?<=patient)\\d+")
-    utils::untar(tgz_element, exdir = tmpdir)
-    tmpdir_patID <- paste0(tmpdir, "/", patID)
-    ## dir of the files within the patID folder, make object patID for subenvironment
-    tmpdir_list <- list.files(paste0(tmpdir, "/", patID))
-    ## make vector of test folders
-    tmpdir_list <- tmpdir_list[!grepl("[[:alpha:]]", tmpdir_list)]
-    ## loop_through_tmpdir will create a list of lists of dataframes for each test in each subfolder
-    loop_through_tmpdir <- function(x) {
-      list_dir <- paste0(tmpdir_patID, "/", x)
-      list_dir_list <- list.files(list_dir)
-      list_dir_xml <- list_dir_list[grepl(".*projection.xml", list_dir_list)]
-      # makes vector of all the xml files with 'projection' but without diff in it
-      list_dir_xml <- list_dir_xml[!grepl("diff", list_dir_xml)]
+  tgz_files <- get_tgz_files(folder = folder)
+  # creates temporary path just for tgz files for unzipping
+  tmpdir <- file.path(tempdir(), "tgzs")
+  ## unzip all tgz files into temporary path
+  lapply(tgz_files, unzip_tgz, tmpdir = tmpdir)
+  ## list all folders in the temporary path
+  tmpdir_list <- list.dirs(tmpdir, full.names = T)
 
+  # XML data
+  ## get all projection files from within all folders
+  ls_xmls <- unlist(lapply(tmpdir_list,
+    function(x) list.files(x, pattern = ".*projection.xml", full.names = TRUE)))
+  ## makes vector of all the xml files with 'projection' but without diff in it
+  ls_xml_nodiff <- ls_xmls[!grepl("diff", ls_xmls)]
+  ## loop core xml extraction over each xml file
+  ls_xml_dfs <- lapply(ls_xml_nodiff, function(x)
+    make_df_each_xml(x, incomplete = incomplete))
+  ## bind rows to one data frame with all xml data
+  ## using bind_rows as dealing with possibly unequal number of colums
+  ## for when some xml entries are missing
+  df_xml <- bind_rows(ls_xml_dfs)
 
-      ## end of function to make data frames looping through xml_projection_list
-      ## happy loop
-      lapply(list_dir_xml, function(x)
-        make_df_each_xml(x, list_dir = list_dir, incomplete = incomplete))
-    } ## end of function loop through tmp_dir
+  # JSON data (PII)
+  ## extract PII from json files and bind it to one data frame
+  list_json <- unlist(lapply(tmpdir_list, function(x) list.files(x, pattern = ".json", full.names = TRUE)))
+  list_pii <- lapply(list_json, extract_pii)
+  df_pii <- do.call(rbind, list_pii)
 
-    lapply(tmpdir_list, loop_through_tmpdir)
-  } ## end of big pull_out() function to use for list of tgz files
+  ## join test data and PII
+  maiadata <- left_join(df_xml, df_pii, by = "patID")
 
-  ## big end picture, result:
-  ## list of list of lists of data frames for each xml for each test folder for each patient folder,
-  ## gives list of list of data frames
-  list_all_exams <- lapply(tgz_name, pull_out)
-
-  # data_all
-  data_coll <-
-    bind_rows(lapply(list_all_exams, function(x) bind_rows(lapply(x, bind_rows)))) %>%
-    rename(stimID = id) %>%
-    arrange(.data$patID, .data$testID, .data$stimID) %>%
-    mutate(testtype = case_when(
-      testtype == "0" ~ "mesopic",
-      testtype == "6" ~ "cyan",
-      testtype == "7" ~ "red",
-      TRUE ~ .data$testtype
-    ))
-  data_test <- data_coll %>%
-    # filter(!testtype %in% "2") %>%
-    distinct(.data$patID, .data$testID, .data$testtype) %>%
-    rename(type = .data$testtype)
-  # return(data_test)
-  # return(data_coll)
-  data_all <- left_join(data_coll, data_test, by = c("patID", 'baseID' = "testID")) %>%
-    mutate(testtype = coalesce(.data$type, .data$testtype),
-           eye = if_else(.data$eye == 'Right', 'r', 'l')) %>%
-    dplyr::select(-"type")
-
-  if (timeclass == "date") {
-    data_all <- data_all %>%
-      dplyr::mutate(testDate = lubridate::as_date(lubridate::ymd_hm(.data$testDate)))
-  } else if (timeclass == "datetime") {
-    data_all <- data_all %>%
-      dplyr::mutate(testDate = lubridate::ymd_hm(.data$testDate))
-  }
-  # use get_names to retrieve sex details of patients. and join with main data frame
-  pat_names <- get_names(folder = folder) %>%
-    dplyr::select('patID', 'sex') %>%
-    dplyr::mutate(patID = as.character(.data$patID))
-
-  data_bind <-
-    data_all %>%
-    dplyr::left_join(pat_names, by = 'patID') %>%
-    dplyr::select('patID', 'sex', everything()) %>%
-    dplyr::mutate(
-      angle = ifelse(.data$eye == 'l', 180- .data$angle, .data$angle),
-      angle = ifelse(sign(.data$angle) == -1,
-                 360 + .data$angle, .data$angle),
-      angle = ifelse(.data$eccent == 0, 0, .data$angle)
-      )
-  data_bind
+  ## clean data frame and flip angles
+  maiadata_clean <- clean_mp_data(maiadata, timeclass = timeclass)
+  ## delete temporary directory
+  unlink(tmpdir, recursive = TRUE)
+  return(maiadata_clean)
 }
+## to be moved to testthat
+# test <-
+#   read_maia_tgz2("/Users/tjebo/_temp_projects/microperimetr/backup_maia-1054-patient662_20231012")
 
-#' @rdname read_maia_tgz
-#' @description core function to create data frame from xml attributes
-#' @param x name of XML file
-#' @param list_dir directory where to find the xml file
+#' clean_mp_data
+#' @description cleaning up extracted data frame with mp test data and PII
+#' @param x data frame
+#' @param timeclass either 'date' (default) or 'datetime' (POSIXct)
 #' @param incomplete set TRUE, if you want to see incomplete exams too
 #' @return Data frame
 #' @import xml2
+#'
+# lookup vector for test type
+lu_test_type <- c(`0` = "mesopic", `1` = "mesopic", `6` = "cyan", `7` = "red")
 
+clean_mp_data <- function(x, timeclass){
+  clean_type <- x %>%
+  dplyr::mutate(
+    # replace integer test type with lookup
+    test_type_new = lu_test_type[test_type],
+    ## add column to identify exam as baseline or follow up
+    ## could be better done by replacing "baselineID" == -1 with actual ID
+    ## or maybe not, there might be incomplete BL exams that have complete FU exms??
+    visit_type = ifelse(test_type == 2, "follow", "base"),
+    ## see above
+    baseID = ifelse(baseID == -1, testID, baseID),
+    # if test is follow up, then replace baseline with lookup
+    base_type_new = ifelse(test_type != 2, NA_character_, lu_test_type[base_type]),
+    ## this can be safely coalesced, as the new columns contain matchin NA and non-NA values
+    test_type = coalesce(.data$test_type_new, .data$base_type_new)) %>%
+    # remove obsolete columns
+    select(-test_type_new, -base_type_new, -base_type) %>%
+    # rename id to stimID
+    rename(stimID = id)
 
-make_df_each_xml <- function(x, list_dir, incomplete) {
-  xmlfile <- xml2::read_xml(paste0(list_dir, "/", x))
+  ## clean date class
+  if(!timeclass %in% c("date", "datetime"))
+    stop("timeclass needs to be one of 'date' or 'datetime'")
+
+  if (timeclass == "date") {
+    clean_date <- clean_type %>%
+      dplyr::mutate(testDate = lubridate::as_date(lubridate::ymd_hm(.data$testDate)))
+  } else if (timeclass == "datetime") {
+    clean_date <- clean_type %>%
+      dplyr::mutate(testDate = lubridate::ymd_hm(.data$testDate))
+  }
+  ## Recode eye characters, flip angles of left eyes
+  clean_angle <-
+    clean_date %>%
+    dplyr::mutate(
+      eye = ifelse(eye == "Left", "l", "r"),
+      angle = ifelse(.data$eye == "l", 180 - .data$angle, .data$angle),
+      angle = ifelse(sign(.data$angle) == -1,
+                 360 + .data$angle, .data$angle),
+      angle = ifelse(.data$eccent == 0, 0, .data$angle)
+      ) %>%
+  ## arrange rows in meaningful way
+    arrange(patID, testID, test_type, stimID) %>%
+  ## arrange columns in meaningful way
+  select("patID", "name", "first_name",
+         "sex", "DOB", "age", "testID", "baseID", "test_type", "visit_type", "eye",
+         "testDate", "avg_rctn", "stimID", "value", "eccent", "angle")
+  return(clean_angle)
+  }
+
+#' @description core function to create data frame from xml attributes
+#' @param x name of XML file
+#' @param incomplete set TRUE, if you want to see incomplete exams too
+#' @return Data frame
+#' @import xml2
+#' @import dplyr
+#' @keywords internal
+#'
+make_df_each_xml <- function(x, incomplete) {
+  xmlfile <- xml2::read_xml(x)
   xml_data <- list(xml2::as_list(xmlfile))[[1]][[1]]
 
   ## Stimuli extraction - define here what variables you want to pull out from the xml
@@ -128,6 +145,7 @@ make_df_each_xml <- function(x, list_dir, incomplete) {
   stimuli_df <- data.frame(t(sapply(l_stimuli, c)),
                            row.names = 1:length(l_stimuli),
                            stringsAsFactors = FALSE) %>%
+    ## to be moved to separate data frame clean up function
     dplyr::mutate(id = as.integer(.data$id), value = as.integer(.data$value),
            eccent = round(as.numeric(.data$eccent),3),
            angle = round(as.numeric(.data$angle),3)) %>%
@@ -139,9 +157,10 @@ make_df_each_xml <- function(x, list_dir, incomplete) {
     patID = "PatientID",
     age = "Age",
     baseID = "ExamBaselineID",
+    base_type = "ExamBaselineType",
     testID = "ExamID",
     Completed = "Completed",
-    testtype = "ExamType",
+    test_type = "ExamType",
     eye = "Eye",
     testDate = "DateTime",
     avg_rctn = "averageReactionTime_ms"
@@ -149,7 +168,13 @@ make_df_each_xml <- function(x, list_dir, incomplete) {
 
   l_df <- nrow(stimuli_df)
   test_attributes <- lapply(list1, function(y) xml_data[[1]][[y]][[1]])
-  attribute_df <- data.frame(t(sapply(test_attributes, c)), stringsAsFactors = FALSE)[rep(1, l_df), ]
+  ## removing NULL entries from list (where xml does not contain info)
+  noNULL_attr <- Filter(function(x) !is.null(x), test_attributes)
+
+  ### the following would probably be more interesting to keep as
+  ## unique identifier separate from test ID values as a list
+  ## should also be in a separate function
+  attribute_df <- data.frame(t(sapply(noNULL_attr, c)), stringsAsFactors = FALSE)[rep(1, l_df), ]
   xml_df <- cbind(attribute_df, stimuli_df)
   row.names(xml_df) <- NULL
   # remove incomplete exams if argument is set to FALSE (default)
@@ -158,12 +183,66 @@ make_df_each_xml <- function(x, list_dir, incomplete) {
       filter(.data$Completed == 1) %>%
       dplyr::select(-'Completed')
   }
+  return(xml_df)
+}
+
+## to be moved to testthat file when to publish in CRAN etc
+# testing on specific XML.
+# Found bug with xml files that might not contain specifit list entry, in particular baseline exam type to cyan and red
+## (probably not required for non scotopic MAIA machines)
+# test_xml <-
+#   make_df_each_xml("/Users/tjebo/_temp_projects/microperimetr/data-raw/backup_maia-1054-patient180_20190401/180/835/projection.xml", incomplete = FALSE)
+# str(test_xml)
+
+#' @description ## List TGZ files
+#' @param folder directory to look in
+#' @keywords internal
+#'
+get_tgz_files <- function(folder) {
+  tgz_files <- list.files(path = folder, pattern = ".tgz", full.names = TRUE)
+  # check if any tgz file exists
+  if (identical(tgz_files, character(0))) {
+    stop("No tgz file in this directory")
+  }
+  tgz_files
 }
 
 #' @rdname read_maia_tgz
-#' @description ## List TGZ files
-#' @param folder directory to look in
+#' @description ## unzip tgz files
+#' @param tgz_file file to extract
+#' @param tmpdir temporary directory
 #'
-get_tgz_files <- function(folder) list.files(path = folder, pattern = ".tgz", full.names = TRUE)
+unzip_tgz <- function(tgz_file, tmpdir) { # tgz_file is element of tgz_files (each tgz file)
+  ## the file name contains the patient ID (pulled here)
+  patID <- stringr::str_extract(tgz_file, "(?i)(?<=patient)\\d+")
+  ## the actual unpacking
+  utils::untar(tgz_file, exdir = tmpdir)
+  ## the tgz file contains an irrelevant sql file and a folder named by the patient ID
+  tmpdir_patID <- paste0(tmpdir, "/", patID)
+}
+
+#' @rdname  read_maia_tgz
+#' @description Extracts personally identifiable information from MAIA raw data.
+#' @details
+#' Internal function to extract name, dob and and sex from MAIA observers.
+#' This data is stored in json files attached to the microperimetry files.
+#' It is generally not recommended to use this function explicitely in order to keep personal identifiable data as safe as possible.
+#' @param x jsonfile
+#' @import dplyr
+#' @importFrom jsonlite fromJSON
+#' @author Tjebo
+#' @return Data frame
+#' @keywords internal
+#'
+extract_pii <- function(x) {
+  ## makes list of json data, contains patient details
+  JsonData <- jsonlite::fromJSON(x)
+  ## list2: what you want to pull out of the .json file
+  list2 <- list(patID = "id", name = "lastname", first_name = "firstname", sex = "gender", DOB = "birthdate")
+  df_json <- data.frame(lapply(list2, function(z) JsonData[[1]][[z]]), stringsAsFactors = FALSE)
+  ## Convert patID to char so that we can join it to XML data
+  df_json$patID <- as.character(df_json$patID)
+  return(df_json)
+}
 
 
